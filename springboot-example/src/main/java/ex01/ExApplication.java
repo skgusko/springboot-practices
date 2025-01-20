9caf9b8a90d6a8127ed81d9e4df35f2b666551e2
Konahyeon <koo@inha.edu>
2025-01-20 16:56:34 +0900

    SpringBoot #1

--- springboot-example/src/main/java/ex01/ExApplication.java
+++ springboot-example/src/main/java/ex01/ExApplication.java
@@ -1,13 +1,35 @@
 package ex01;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExApplication {
+public class ExApplication { // Bootstrapping Class
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExApplication.class, args);
+	@Bean
+	public Map<?,?> myMap() {
+		return Map.of("greetings", "Hello World");
 	}
-
+	public static void main(String[] args) {
+		/*
+		ConfigurableApplicationContext ac = null;
+		try {
+			//ApplicationRunner 인터페이스를 구현한 빈을 Application Context에서 찾아서 실행 - ex02.ExApplication
+			ac = SpringApplication.run(ExApplication.class, args);
+			System.out.println("ExApplication runs..." + ac.getBean("m", Map.class).get("greetings"));
+		} catch(Exception ignored) {
+			
+		} finally {
+			ac.close();
+		}
+		*/
+		
+		try(ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args);) {
+			System.out.println("ExApplication runs..." + ac.getBean("m", Map.class).get("greetings"));
+		}
+	}
 }
