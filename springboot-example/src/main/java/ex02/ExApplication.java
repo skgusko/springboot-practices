e64deb0ea704921348ddc07b065203a6ad9ffb81
Konahyeon <koo@inha.edu>
2025-01-21 10:17:26 +0900

    spring boot #4

---
+++ springboot-example/src/main/java/ex02/ExApplication.java
@@ -0,0 +1,31 @@
+package ex02;
+
+import java.util.Map;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import ex02.runner.HelloWorldRunner;
+
+@SpringBootApplication
+public class ExApplication { // Bootstrapping Class
+
+	@Bean
+	public Map<?,?> myMap() {
+		return Map.of("greetings", "Hello World");
+	}
+	
+	@Bean
+	public ApplicationRunner applicationRunner(Map<?,?> map) {
+		return new HelloWorldRunner(myMap());
+	}
+	
+	public static void main(String[] args) {
+		try(ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args);) {
+			
+		}
+	}
+}
