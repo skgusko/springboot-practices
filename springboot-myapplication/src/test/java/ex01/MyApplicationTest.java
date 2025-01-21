17ab0afcab1158cc538d3f9b7e10866aefbc907f
Konahyeon <koo@inha.edu>
2025-01-21 11:14:56 +0900

    spring boot #4

---
+++ springboot-myapplication/src/test/java/ex01/MyApplicationTest.java
@@ -0,0 +1,35 @@
+package ex01;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+public class MyApplicationTest {
+	
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Test
+	public void testMyComponent01NotNull() {
+		MyComponent myComponent = applicationContext.getBean("MyComponent01", MyComponent.class);
+		assertNotNull(myComponent);
+	}
+	
+	@Test
+	public void testMyComponent02Null() {
+//		MyComponent myComponent = applicationContext.getBean("MyComponent02", MyComponent.class);
+//		assertNull(myComponent);
+		
+		// @SpringBootTest 에서는 @Configuration을 달고 있는 설정 클래스를 찾지 못한다.
+		assertThrows(NoSuchBeanDefinitionException.class, () -> {
+			applicationContext.getBean("MyComponent02", MyComponent.class);
+		});
+	}
+}
